a3963a78c986ec395236f092d55dbc6d631db54c
khlam <4841220+khlam@users.noreply.github.com>
2021-12-17 17:22:25 -0800

    using https://www.npmjs.com/package/@koenie06/discord.js-music?activeTab=readme

--- main.js
+++ main.js
@@ -1,26 +1,64 @@
 const { Client, Intents } = require('discord.js')
 
 const s = require('./src/sanitizer')
-
+const voice = require('./src/voice')
+const music = require('./src/music')
 const yt = require('./src/ytplayer')
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS,
     Intents.FLAGS.GUILD_MESSAGES, // read messages
-    Intents.FLAGS.GUILD_VOICE_STATES // check voice connection
+    Intents.FLAGS.GUILD_VOICE_STATES // voice connection
 ] })
 
 const prefix = "-"
 
 let command = null
 
+let voice_connection
+let voice_channel
+
+let guild_id
+
+async function connect_to_voice(msg) {
+    voice_channel = msg.member.voice.channel
+    guild_id = voice_channel.guild.id
+    if (voice_channel) {
+        voice_connection = await voice.connect_to_channel(msg.member.voice.channel)
+        if (voice_connection) {
+            console.log("Joined Voice Channel", voice_channel.name, guild_id)
+        }else {
+            msg.reply("ERROR: Could not connect to voice channel.")
+        }
+    }
+}
+
 async function execute(cmd, msg){
     return new Promise(async(resolve, reject) => {
-        if ((cmd.startsWith("p") === true) || cmd.startsWith("play")) {
-            await yt.connect_to_channel(msg.member.voice.channel)
-            resolve(true)
-        }
+        let result = false
+        
+        cmd = cmd.split(" ")
 
-        resolve(true)
+        cmd_header = cmd[0].replace(prefix, "")
+
+        cmd = cmd[cmd.length - 1]
+
+        if ((cmd_header === "p") || (cmd_header === "play")) {
+            let _ytObj = await yt.get_youtube_obj(cmd)
+            console.log("here")
+            if (_ytObj !== false) {
+                music.play({
+                    interaction: msg,
+                    channel: msg.member.voice.channel,
+                    song: _ytObj.address
+                })
+                msg.channel.send(`*${_ytObj.name}*\t \`${_ytObj.length}\``)
+            }
+        }else if ( (cmd_header === "s") || cmd_header === "skip") {
+            music.skip({
+                interaction: msg
+            })
+        }
+        resolve(result)
     })
 }
 
@@ -34,7 +72,7 @@ client.on("messageCreate", async (msg) => {
     if (msg.content.startsWith(prefix)) {
         console.log("o > '", msg.content, "'")
         
-        command = s.sanitize(msg.content)
+        command = msg.content
         
         console.log("s > '", command, "'")
         
--- src/ytplayer.js
+++ src/ytplayer.js
@@ -1,27 +1,31 @@
-const { joinVoiceChannel, createAudioPlayer, createAudioResource, StreamType, entersState, VoiceConnectionStatus, AudioPlayerStatus } = require('@discordjs/voice')
+const ytdl = require('ytdl-core')
 
-const delay = ms => new Promise(resolve => setTimeout(resolve, ms))
-
-// src: https://www.npmjs.com/package/@koenie06/discord.js-music
-async function connect_to_channel(channel) {
-    return new Promise(async(resolve, reject) => {
-        const connection = joinVoiceChannel({
-            channelId: channel.id,
-            guildId: channel.guild.id,
-            adapterCreator: channel.guild.voiceAdapterCreator,
-            selfDeaf: false
-        })
-
-        connection.once(VoiceConnectionStatus.Ready, () => {
-            console.log("Connected to voice channel '", channel.name, "'")
-            resolve(connection)
-        })
-
-        await delay(3000)
-        reject('Connection timeout')
-    })
+async function get_youtube_obj(URL) {
+    if(ytdl.validateURL(URL) === true) {
+        try {
+            let info = await ytdl.getInfo(URL)
+            let _videoObj = {
+                name: `${info.videoDetails.title.substring(0, 30)}...`,
+                length: new Date(info.videoDetails.lengthSeconds * 1000).toISOString().substr(11, 8),
+                type: "yt",
+                address: URL
+            }
+            return _videoObj
+        }catch(e) {
+            console.log(`${e}`)
+            let _videoObj = {
+                name: `${URL.substring(5, 15)}`,
+                length: "ERR",
+                type: "yt",
+                address: URL
+            }
+            return _videoObj
+        }
+    }else {
+        return false
+    }
 }
 
 module.exports = {
-    connect_to_channel
+    get_youtube_obj
 }
